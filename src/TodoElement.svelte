583962ddd3797939fc418597011fd7cc70309014
myoun <aiden080605@gmail.com>
2022-02-01 00:10:15 +0900

    bug fix

--- src/TodoElement.svelte
+++ src/TodoElement.svelte
@@ -1,11 +1,4 @@
 <script lang="ts">
-    function click(e) {
-        if ((e.target as HTMLInputElement).checked) {
-            document.getElementById("value").style.textDecoration = "line-through";
-        } else {
-            document.getElementById("value").style.textDecoration = "none";
-        }
-    }
     function del() {
         document.getElementById("todoelem").remove()
     } 
@@ -13,6 +6,7 @@
 </script>
 
 <style>
+    
     #todoelem {
         height : 50px;
         font-family: D2Coding;
@@ -39,15 +33,16 @@
     #todoelem:hover #delete {
         display: initial;
     }
-    #delete button {
-        height : 30px;
-        width : 30px;
+
+    #cb:checked + #value {
+        text-decoration: line-through;
+        color: gray;
     }
 </style>
 
 <link href="http://cdn.jsdelivr.net/gh/joungkyun/font-d2coding/d2coding.css" rel="stylesheet" type="text/css">
 <div id="todoelem">
-    <input type="checkbox" on:click="{click}">
+    <input id="cb" type="checkbox">
     <span id="value">{value}</span>
     <button id="delete" on:click="{del}">
         <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMyA2djE4aDE4di0xOGgtMTh6bTUgMTRjMCAuNTUyLS40NDggMS0xIDFzLTEtLjQ0OC0xLTF2LTEwYzAtLjU1Mi40NDgtMSAxLTFzMSAuNDQ4IDEgMXYxMHptNSAwYzAgLjU1Mi0uNDQ4IDEtMSAxcy0xLS40NDgtMS0xdi0xMGMwLS41NTIuNDQ4LTEgMS0xczEgLjQ0OCAxIDF2MTB6bTUgMGMwIC41NTItLjQ0OCAxLTEgMXMtMS0uNDQ4LTEtMXYtMTBjMC0uNTUyLjQ0OC0xIDEtMXMxIC40NDggMSAxdjEwem00LTE4djJoLTIwdi0yaDUuNzExYy45IDAgMS42MzEtMS4wOTkgMS42MzEtMmg1LjMxNWMwIC45MDEuNzMgMiAxLjYzMSAyaDUuNzEyeiIvPjwvc3ZnPg==">
